f36219be957365249886073d1abe6231babeb19c
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-11-03 12:04:40 +0100

    saved

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
-import pandas as pd 
+
+
 
 # functions
+
+#define your javascript 
+my_js = """
+alert("hello wrld");
+"""
 
 # streamlit app
 
 st.title("hello world")
 
 st.text('text')
+
